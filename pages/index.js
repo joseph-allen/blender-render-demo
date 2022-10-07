0ed93897833d3abac083fa6c77d6d128c6bc97a1
Joe <joseph.allen.1994@hotmail.com>
2022-10-07 23:11:15 +0900

    Add general Box demo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Canvas } from "@react-three/fiber";
+import Box from "../components/Box";
+import LightBulb from "../components/LightBulb";
+import Floor from "../components/Floor";
+import OrbitControls from "../components/OrbitControls";
+import Draggable from "../components/Draggable";
 
 export default function Home() {
   return (
@@ -13,57 +18,26 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Blender Render Demo in 3.js
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <div className={styles.scene}>
+          <Canvas
+            shadows={true}
+            className={styles.canvas}
+            camera={{
+              position: [-6, 7, 7],
+            }}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <ambientLight color='red' intensity={0.3}/>
+            <LightBulb position={[0, 3, 0]} />
+            <Draggable>
+              <Box rotateX={3} rotateY={0.2} />
+            </Draggable>
+            <OrbitControls />
+            <Floor position={[0, -1, 0]} />
+          </Canvas>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
